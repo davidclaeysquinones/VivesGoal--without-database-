38d56f1f36146438b380dbdc14088240ecf79279
davidclaeysquinones <davidclaeys@outlook.com>
2015-12-17 08:35:23 +0100

    update

---
+++ test/appTest/TestSuite.java
@@ -0,0 +1,42 @@
+package appTest;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author david
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({appTest.PersoonDBTest.class, appTest.PloegDBTest.class, appTest.PloegTransTest.class})
+public class TestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
